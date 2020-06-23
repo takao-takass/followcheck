e5241de6ba61f14302c1e1f769277b71ea66cc53
多田昂央 <takass0119@yahoo.co.jp>
2020-06-23 23:11:45 +0900

    pythonパス修正

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -4,5 +4,5 @@
 LOG_YMD=`date '+%Y%m%d'`
 
 cd /opt/followcheck/scripts
-/usr/local/bin/python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
-/usr/local/bin/python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
+/usr/bin/python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
+/usr/bin/python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
