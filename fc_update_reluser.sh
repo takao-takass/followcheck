4b851077c8cdafacf378fc7fdc4f696d196fa95b
多田昂央 <takass0119@yahoo.co.jp>
2020-06-27 00:10:04 +0900

    プロセスの順番を入れ替える

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -4,6 +4,6 @@
 LOG_YMD=`date '+%Y%m%d'`
 
 cd /opt/followcheck/scripts
-/usr/bin/python3.7 ./verify_reluser.py >> ../log/update_reluser_$LOG_YMD.log
 /usr/bin/python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
+/usr/bin/python3.7 ./verify_reluser.py >> ../log/update_reluser_$LOG_YMD.log
 /usr/bin/python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
