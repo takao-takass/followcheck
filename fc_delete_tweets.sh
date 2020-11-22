5bc2aaad9c0f650f2d4e11501de2df82d3abde4d
多田昂央 <takass0119@yahoo.co.jp>
2020-11-22 23:42:56 +0900

    使用しなくなったバッチファイルを削除

--- fc_delete_tweets.sh
+++
@@ -1,8 +0,0 @@
-#
-
-LOGFILE_NAME=delete_tweets_`date '+%Y%m%d'`.log
-
-cd /opt/followcheck/scripts
-echo `date '+%Y-%m-%d %H:%M:%S'` start#################### >> ../log/$LOGFILE_NAME
-python3.7 delete_tweets.py >> ../log/$LOGFILE_NAME
-
