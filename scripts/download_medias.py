8c9cec3089a5cf5478987dc0c66d20e2d6915e71
多田昂央 <takass0119@yahoo.co.jp>
2020-12-09 01:09:13 +0900

    WIP
    #1

--- scripts/download_medias.py
+++ scripts/download_medias.py
@@ -1,4 +1,7 @@
+import os
 import sys, hashlib, config, cv2
+import urllib
+
 from PIL import Image
 from classes import logger, thread, databeses, exceptions
 
@@ -60,22 +63,65 @@ class DownloadMedias:
                 }
             )
 
+            log.info("ダウンロード件数：" + str(len(download_medias)))
             for download_media in download_medias:
-                
+
                 splited_usls = download_media['url'].split('/')
-                filename = splited_usls[len(splited_usls)-1].split('?')[0]
+                file_name = splited_usls[len(splited_usls) - 1].split('?')[0]
+
+                # ディレクトリパス（無ければ作る）
+                directory_path = config.STRAGE_MEDIAS_PATH + download_media['disp_name'] + '/'
+                if not os.path.exists(directory_path):
+                    os.mkdir(directory_path)
+
+                # 画像ファイルの対応サイズを判定
+                size = "thumb"
+                if "large" in download_media['sizes']:
+                    size = "large"
+                elif "medium" in download_media['sizes']:
+                    size = "medium"
+                elif "small" in download_media['sizes']:
+                    size = "small"
+
+                try:
+                    print("ダウンロード中...  " + download_media['url'])
+                    data = urllib.request.urlopen(download_media['url'] + ":" + size, timeout=10).read()
+                    with open(directory_path + file_name, mode="wb") as f:
+                        f.write(data)
+
+                    db.execute(
+                        " UPDATE tweet_medias"
+                        " SET file_name = %(file_name)s"
+                        "    ,directory_path = %(directory_path)s"
+                        " WHERE tweet_id = %(tweet_id)s"
+                        " AND url = %(url)s",
+                        {
+                            'file_name': file_name,
+                            'directory_path': directory_path,
+                            'tweet_id': download_media['tweet_id'],
+                            'url': download_media['url']
+                        }
+                    )
+                    db.execute(
+                        " INSERT INTO queue_create_thumbs (tweet_id, url)"
+                        " VALUES ( %(tweet_id)s, %(url)s)",
+                        {
+                            'tweet_id': download_media['tweet_id'],
+                            'url': download_media['url']
+                        }
+                    )
+                    db.execute(
+                        " INSERT INTO queue_compress_medias (tweet_id, url)"
+                        " VALUES ( %(tweet_id)s, %(url)s)",
+                        {
+                            'tweet_id': download_media['tweet_id'],
+                            'url': download_media['url']
+                        }
+                    )
 
 
-
-
-                # download_mediaws_bk.py および create_thumbnail.py を参考に実装する。
-
-
-
-
-
-
-
+                except Exception as e:
+                    log.error(e)
 
 
         except exceptions.UncreatedThreadException:
