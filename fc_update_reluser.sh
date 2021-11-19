505656ba210ba1688811a6b0df442e735926424b
多田昂央 <takass0119@yahoo.co.jp>
2021-11-20 04:03:05 +0900

    s

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 . ./enviroments.conf
+echo $ENDPOINT
+echo $BATCH_TOKEN
+echo $LOG_YMD
 
 curl -X $ENDPOINT/followcheck/batch/UpdateUsers?token=$BATCH_TOKEN > /opt/followcheck/log/update_reluser_$LOG_YMD.log
