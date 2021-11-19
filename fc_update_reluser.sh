240fa22f67466c86e2c6b90ad84e4cecd2f9c7de
多田昂央 <takass0119@yahoo.co.jp>
2021-11-20 04:15:51 +0900

    wip

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 . ./enviroments.conf
-echo $ENDPOINT
-echo $BATCH_TOKEN
-echo $LOG_TIMESTAMP
 
-URL=`$ENDPOINT'/followcheck/batch/UpdateUsers?token='$BATCH_TOKEN`
-LOG=`/opt/followcheck/log/update_reluser_$LOG_TIMESTAMP.log`
-echo $URL
-echo $LOG
+URL=$ENDPOINT'/followcheck/batch/UpdateUsers?token='$BATCH_TOKEN
+LOG=/opt/followcheck/log/update_reluser_$LOG_TIMESTAMP.log
 
 curl -X POST $URL > $LOG
