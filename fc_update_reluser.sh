a22efc624afb604e6c7a2729645ee28b0c1319e3
多田昂央 <takass0119@yahoo.co.jp>
2021-11-20 03:49:12 +0900

    change batch endpoint.

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -1,9 +1,11 @@
 # followcheck batch file
 # 関連ユーザ情報更新
 
-LOG_YMD=`date '+%Y%m%d'`
+LOG_YMD=`date '+%Y%m%d_%H%M%S'`
 
-cd /opt/followcheck/scripts
-python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
-python3.7 ./verify_reluser.py >> ../log/update_reluser_$LOG_YMD.log
-python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
+curl -X https://takas-service.webhop.net:10082/followcheck/batch/UpdateUsers?token=folloWcheck_BatCh_01092123 > /opt/followcheck/log/update_reluser_$LOG_YMD.log
+
+#cd /opt/followcheck/scripts
+#python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
+#python3.7 ./verify_reluser.py >> ../log/update_reluser_$LOG_YMD.log
+#python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
