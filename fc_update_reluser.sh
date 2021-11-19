8607bb815895cb4d387897714851745a2259a9e3
多田昂央 <takass0119@yahoo.co.jp>
2021-11-20 04:01:14 +0900

    w

--- fc_update_reluser.sh
+++ fc_update_reluser.sh
@@ -1,11 +1,5 @@
-# followcheck batch file
-# 関連ユーザ情報更新
+#!/bin/sh
 
-LOG_YMD=`date '+%Y%m%d_%H%M%S'`
+. ./enviroments.conf
 
-curl -X https://takas-service.webhop.net:10082/followcheck/batch/UpdateUsers?token=folloWcheck_BatCh_01092123 > /opt/followcheck/log/update_reluser_$LOG_YMD.log
-
-#cd /opt/followcheck/scripts
-#python3.7 ./update_reluser.py >> ../log/update_reluser_$LOG_YMD.log
-#python3.7 ./verify_reluser.py >> ../log/update_reluser_$LOG_YMD.log
-#python3.7 ./update_profileicon.py >> ../log/update_reluser_$LOG_YMD.log
+curl -X $ENDPOINT/followcheck/batch/UpdateUsers?token=$BATCH_TOKEN > /opt/followcheck/log/update_reluser_$LOG_YMD.log
